25ce32893418473cf5130a37fd0a32ac21925489
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-09-11 15:41:02 -0300

    Criei o middleware que verifica o token enviado para fazer login do usuário

---
+++ src/middlewares/loginRequired.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const jwt = require('jsonwebtoken')
+
+function loginRequired (req, res, next) {
+    const { authorization } = req.headers
+
+    if(!authorization) {
+        return res.status(401).json('Faça login')
+    }
+
+    const [ , token ] = authorization.split(' ')
+
+    try {
+        const decoded = jwt.verify(token, process.env.PRIVATEKEY)
+
+        const { id, email } = decoded
+
+        req.userId = id
+        req.userEmail = email
+
+        next()
+    } catch(e) {
+        return res.status(401).json('TOKEN expirado ou inválido')
+    }
+}
+
+module.exports = loginRequired
--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
 
+const loginRequired = require('../middlewares/loginRequired')
 const categoriesController = require('../controllers/CategoriesController')
 
 const route = new Router()
 
-route.post('/', categoriesController.create)
+route.post('/', loginRequired, categoriesController.create)
 route.get('/', categoriesController.index)
 route.get('/:id', categoriesController.show)
 route.put('/', categoriesController.update)
