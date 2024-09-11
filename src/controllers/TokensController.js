50840670c25fa6432ca949088573bdce033f4723
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-09-11 01:55:23 -0300

    Exclui o TokenModel pois não ha necessidade de salvar o token no banco de dados

--- src/controllers/TokensController.js
+++ src/controllers/TokensController.js
@@ -1,8 +1,6 @@
 const bcrypt = require('bcryptjs')
-const isEmail = require('validator/lib/isEmail')
 
 const UsersModel = require('../models/UsersModel')
-const TokensModel = require('../models/TokensModel')
 
 class TokensController {
     async create(req, res) {
@@ -19,13 +17,10 @@ class TokensController {
 
         if(!passwordIsValid) return res.status(401).json('Senha incorreta')
 
-        req.body.id = user.id
-
-        const token = new TokensModel(req.body)
-        const tokenCreated = await token.create()
+        const token = jwt.sign({ id: user.id, email: user.email }, process.env.PRIVATEKEY, {expiresIn: 60 * 60 * 24 * 7})
 
         return res.status(200).json({
-            token: tokenCreated,
+            token: token,
             user: {
                 id: user.id,
                 name: user.name,
