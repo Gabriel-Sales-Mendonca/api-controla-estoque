775cc7fd7b5170087c37a2f4aa6778b75d6776c3
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-09-30 12:59:31 -0300

    Adicionei o cors para o meu frontend usar essa API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const homeRoutes = require('./src/routes/homeRoutes')
 const productsRoutes = require('./src/routes/productsRoutes')
@@ -27,6 +28,8 @@ class App {
     }
 
     middlewares() {
+        this.app.use(cors())
+        this.app.use(express.urlencoded({ extended: true }))
         this.app.use(express.json())
     }
 
